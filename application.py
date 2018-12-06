74e85677a82175db3d51859edfec8f234e35f591
hadeel_kh <doooly605@gmail.com>
2018-12-06 17:15:08 +0300

    use decorators for authentication check

--- application.py
+++ application.py
@@ -16,6 +16,7 @@ import string
 import httplib2
 import json
 import requests
+from functools import wraps
 
 auth = HTTPBasicAuth()
 
@@ -55,13 +56,13 @@ def fbconnect():
      'web']['app_id']
     app_secret = json.loads(open(
         'fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)  # NOQA
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
-    # Use token to get user info from API
+    """Use token to get user info from API"""
     userinfo_url = "https://graph.facebook.com/v2.8/me"
-    '''
+    """
     Due to the formatting for the result from the server
     token exchange we have to split the token
     first on commas and select the first index
@@ -69,33 +70,33 @@ def fbconnect():
     we split it on colons to pull out the actual token value
     and replace the remaining quotes with nothing
     so that it can be used directly in the graph api calls
-    '''
+    """
     token = result.split(',')[0].split(':')[1].replace('"', '')
 
-    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
+    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token  # NOQA
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
-    # print "url sent for API access:%s"% url
-    # print "API JSON result: %s" % result
+    """Print "url sent for API access:%s"% url
+        Print "API JSON result: %s" % result"""
     data = json.loads(result)
     login_session['provider'] = 'facebook'
     login_session['username'] = data["name"]
     login_session['email'] = data["email"]
     login_session['facebook_id'] = data["id"]
 
-    # token must be stored in login_session to properly logout
+    """Token must be stored in login_session to properly logout"""
     login_session['access_token'] = token
 
-    # Get user picture
-    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
+    """Get user picture"""
+    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token  # NOQA
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
 
     login_session['picture'] = data["data"]["url"]
 
-    # see if user exists
+    """See if user exists"""
     user_id = getUserID(login_session['email'])
     if not user_id:
             user_id = createUser(login_session)
@@ -116,12 +117,13 @@ def fbconnect():
     return output
 
 
+# Facebook disconnect.
 @app.route('/fbdisconnect')
 def fbdisconnect():
     facebook_id = login_session['facebook_id']
-    # The access token must me included to successfully logout
+    """The access token must me included to successfully logout"""
     access_token = login_session['access_token']
-    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id, access_token)
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id, access_token)  # NOQA
     h = httplib2.Http()
     result = h.request(url, 'DELETE')[1]
     return "you have been logged out"
@@ -129,7 +131,7 @@ def fbdisconnect():
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
-    # Validate state token
+    """Validate state token."""
     if request.args.get('state') != login_session['state']:
             response = make_response(
                 json.dumps('Invalid state parameter.'), 401)
@@ -139,7 +141,7 @@ def gconnect():
     code = request.data
 
     try:
-        # Upgrade the authorization code into a credentials object
+        """Upgrade the authorization code into a credentials object"""
         oauth_flow = flow_from_clientsecrets(
             'client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
@@ -150,19 +152,19 @@ def gconnect():
             response.headers['Content-Type'] = 'application/json'
             return response
 
-    # Check that the access token is valid.
+    """Check that the access token is valid."""
     access_token = credentials.access_token
-    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)  # NOQA
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
-    # If there was an error in the access token info, abort.
+    """If there was an error in the access token info, abort."""
     if result.get('error') is not None:
         response = make_response(
             json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Verify that the access token is used for the intended user.
+    """Verify that the access token is used for the intended user."""
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
         response = make_response(json.dumps(
@@ -170,7 +172,7 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Verify that the access token is valid for this app.
+    """Verify that the access token is valid for this app."""
     if result['issued_to'] != CLIENT_ID:
         response = make_response(json.dumps(
             "Token's client ID does not match app's."), 401)
@@ -186,11 +188,11 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Store the access token in the session for later use.
+    """Store the access token in the session for later use."""
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
 
-    # Get user info
+    """Get user info"""
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
@@ -199,10 +201,10 @@ def gconnect():
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
-    # ADD PROVIDER TO LOGIN SESSION
+    """ADD PROVIDER TO LOGIN SESSION"""
     login_session['provider'] = 'google'
 
-    # see if user exists, if it doesn't make a new one
+    """See if user exists, if it doesn't make a new one"""
     user_id = getUserID(data["email"])
     if not user_id:
         user_id = createUser(login_session)
@@ -221,8 +223,8 @@ def gconnect():
     return output
 
 
-# User Helper Functions
 def createUser(login_session):
+    """User Helper Functions"""
     newUser = User(
         name=login_session['username'], email=login_session['email'],
         picture=login_session['picture'])
@@ -246,17 +248,29 @@ def getUserID(email):
             return None
 
 
+def login_required(func):
+    """Make sure user is logged in before proceeding"""
+
+    @wraps(func)
+    def x(*args, **kwargs):
+        if 'username' not in login_session:
+            return redirect('/login')
+        return func(*args, **kwargs)
+    return x
+
+
 # DISCONNECT - Revoke a current user's token and reset their login_session
 @app.route('/gdisconnect')
 def gdisconnect():
     # Only disconnect a connected user.
+
     access_token = login_session.get('access_token')
     if access_token is None:
         response = make_response(
             json.dumps('Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token  # NOQA
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
     if result['status'] == '200':
@@ -297,9 +311,8 @@ def disconnect():
 # JSON APIs to view categories
 @app.route('/Categories/JSON')
 @app.route('/catalog/JSON')
+@login_required
 def CategoriesJSON():
-    if 'username' not in login_session:
-        return redirect('/login')
     categories = session.query(Categories)
     return jsonify(category=[r.serialize for r in categories])
 
@@ -307,9 +320,8 @@ def CategoriesJSON():
 # JSON APIs to view category items
 @app.route('/catalog/<int:category_id>/JSON')
 @app.route('/Categories/<int:category_id>/items/JSON')
+@login_required
 def CategoryItemJSON(category_id):
-    if 'username' not in login_session:
-        return redirect('/login')
     Items = session.query(
         CatItems).filter_by(category_id=category_id).all()
     return jsonify(category=[r.serialize for r in Items])
@@ -353,9 +365,8 @@ def showItemDetails(categoryName, Item):
 
 # add new Item
 @app.route('/Categories/item/new/', methods=['GET', 'POST'])
+@login_required
 def newItem():
-    if 'username' not in login_session:
-        return redirect('/login')
     categories = session.query(Categories)
     if request.method == 'POST':
         newitem = CatItems(
@@ -370,19 +381,17 @@ def newItem():
 
 
 # edit item
-@app.route(
-    '/Categories/<string:categoryName>/<int:Item>/edit/', methods=[
+@app.route('/Categories/<string:categoryName>/<int:Item>/edit/', methods=[
         'GET', 'POST'])
+@login_required
 def editItem(categoryName, Item):
-    if 'username' not in login_session:
-        return redirect('/login')
     editedItem = session.query(CatItems).filter_by(id=Item).one()
     categories = session.query(Categories)
     if editedItem.user_id != login_session['user_id']:
-        return """<script>
-            function myFunction(){
-                alert('You are not authorized to edit this item.
-                 Please create your own item in order to edit.');}
+        return """<script> \
+            function myFunction(){ \
+                alert('You are not authorized to edit this item.\
+                 Please create your own item in order to edit.');}\
             </script><body onload='myFunction()'>"""
     if request.method == 'POST':
         if request.form['title']:
@@ -405,15 +414,14 @@ def editItem(categoryName, Item):
 
 # Delete an item
 @app.route('/Categories/<int:Item>/delete/', methods=['GET', 'POST'])
+@login_required
 def deleteItem(Item):
-    if 'username' not in login_session:
-        return redirect('/login')
     deleteItem = session.query(CatItems).filter_by(id=Item).one()
     if deleteItem.user_id != login_session['user_id']:
-        return """<script>
-            function myFunction(){
-                alert('You are not authorized to delete this item.
-                 Please create your own item in order to delete.');}
+        return """<script> \
+            function myFunction(){ \
+                alert('You are not authorized to delete this item.\
+                 Please create your own item in order to edit.');}\
             </script><body onload='myFunction()'>"""
     if request.method == 'POST':
         session.delete(deleteItem)
